61075b75608e47d0047f8da0d6590b2ef407cff5
kedar <kedardasnew@gmail.com>
2022-03-08 12:23:22 +0530

    add demo.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+import os
+import time
+from selenium import webdriver
+
+chromedriver = "C:/SELENIUM DRIVERS/CD/chromedriver.exe"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
+driver.get("https://www.google.com/")
+
