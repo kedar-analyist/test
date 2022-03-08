ea84c37615ee449aa1282c1e2218da30df03ee90
kedar <kedardasnew@gmail.com>
2022-03-08 12:41:17 +0530

    modified demo.py

--- demo.py
+++ demo.py
@@ -6,4 +6,4 @@ chromedriver = "C:/SELENIUM DRIVERS/CD/chromedriver.exe"
 os.environ["webdriver.chrome.driver"] = chromedriver
 driver = webdriver.Chrome(chromedriver)
 driver.get("https://www.google.com/")
-
+print("Testcase Success")
